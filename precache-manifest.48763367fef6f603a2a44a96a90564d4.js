2c1b069990de5dc6853436861068a61c2c4b0351
Marshall Ku <marshall@kakao.com>
2020-05-18 21:09:43 +0900

    Add files via upload

---
+++ precache-manifest.48763367fef6f603a2a44a96a90564d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715999fd43686047a0e3590017c1b522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "981f79108e66f172ac6b",
+    "url": "/static/css/main.2d516bb0.chunk.css"
+  },
+  {
+    "revision": "1462c0f1a886d98b42e1",
+    "url": "/static/js/2.64f2f55f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64f2f55f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981f79108e66f172ac6b",
+    "url": "/static/js/main.4c86903c.chunk.js"
+  },
+  {
+    "revision": "f638d4cacc79e2f70085",
+    "url": "/static/js/runtime-main.0d6e1bf4.js"
+  }
+]);
